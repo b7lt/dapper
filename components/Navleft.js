d26c38e62a9648d4509d97e3931e73e1e1178cdb
Ariel <arielmich64@gmail.com>
2025-04-21 23:14:10 -0400

    init repo

---
+++ components/Navleft.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export default function Navleft() {
+    return(
+        <Nav>
+            aa
+        </Nav>
+    )
+}
+
+const Nav = styled.nav`
+display: flex;
+align-items: center;
+padding-top: 15px;
+padding-bottom: 15px;
+text-decoration: none;
+`
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import "@/styles/globals.css";
+import Head from "next/head";
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Verdana;
+}
+`
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return(
+    <>
+      <Head>
+        <title>Dapper Social</title>
+        <meta name='description' content='Decentralized social media network'></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
